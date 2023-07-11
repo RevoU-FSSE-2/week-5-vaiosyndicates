096d3bdaeafee9f54054c239914557fc468d78c8
kresna <kresnafti2013@gmail.com>
2023-07-11 18:02:04 +0700

    first init

---
+++ public/assets/js/slider/slider.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  console.log('ready')
+  $('.brand-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:5
+      }
+    }
+  }) 
+  
+})
