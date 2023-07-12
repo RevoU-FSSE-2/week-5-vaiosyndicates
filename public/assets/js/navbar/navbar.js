06f1b44b2bd5ff3c71627f96c2265c47fc932626
kresna <kresnafti2013@gmail.com>
2023-07-12 17:58:45 +0700

    responsive for mobile

---
+++ public/assets/js/navbar/navbar.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  console.log('ready nav')
+  $('#mobile-open').on('click', (e) => {
+    e.preventDefault();
+    $('#mobile-close').css("display", "block")
+    $('#mobile-open').css("display", "none")
+    $('#aside').fadeOut(200, function(){
+      $(this).css("display", "block");
+    })
+  })
+
+  $('#mobile-close').on('click', (e) => {
+    e.preventDefault();
+    $('#mobile-close').css("display", "none")
+    $('#mobile-open').css("display", "block")
+    $('#aside').fadeOut(200, function(){
+      $(this).css("display", "none");
+    })
+  })
+})
